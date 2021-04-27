18def9c8b80641dafe3b32977e7455fda08641ee
Jinhei <nicholas.jinhei.fong@gmail.com>
2021-04-26 22:51:56 -0400

    improve ui, allow selection of nanoleaf state mapping to discord

--- electron/services/electron.js
+++ electron/services/electron.js
@@ -2,6 +2,6 @@ const { ipcMain, BrowserWindow } = require('electron');
 
 ipcMain.handle('electron-icon-overlay', (event, icon) => {
   BrowserWindow.getAllWindows().forEach((w) => {
-    w.setOverlayIcon(icon ? `./assets/icons/${icon}.png` : null, icon || '');
+    w.setOverlayIcon(icon ? `./assets/images/${icon}.png` : null, icon || '');
   });
 });
--- src/components/Nanoleaf/Nanoleaf.js
+++ src/components/Nanoleaf/Nanoleaf.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import cx from 'classnames';
+
 import React, { useContext, useEffect, useState } from 'react';
 import { DiscordContext } from '../../context/Discord';
 
@@ -14,6 +16,12 @@ const NANOLEAF_SCENES = {
 const Nanoleaf = () => {
   const [ready, setReady] = useState(false);
   const [scenes, setScenes] = useState([]);
+  const [muteScene, setMuteScene] = useState(NANOLEAF_SCENES.MUTED);
+  const [unmuteScene, setUnmuteScene] = useState(NANOLEAF_SCENES.NOT_MUTED);
+  const [defaultScene, setDefaultScene] = useState(NANOLEAF_SCENES.NOT_IN_CALL);
+
+  const [selectionFn, setSelectionFn] = useState(null);
+  console.log({ selectionFn, setDefaultScene, muteScene, unmuteScene, defaultScene });
 
   useEffect(() => {
     // TODO: allow user to input credentials
@@ -33,35 +41,106 @@ const Nanoleaf = () => {
     let scene;
     let icon;
     if (!voiceChannelId) {
-      scene = NANOLEAF_SCENES.NOT_IN_CALL;
+      scene = defaultScene;
       icon = null;
     } else if (
       voiceSettings.mute
       || voiceSettings.deaf
       || _.get(voiceSettings, 'mode.type') === 'PUSH_TO_TALK'
     ) {
-      scene = NANOLEAF_SCENES.MUTED;
+      scene = muteScene;
       icon = 'mic_mute';
     } else {
-      scene = NANOLEAF_SCENES.NOT_MUTED;
+      scene = unmuteScene;
       icon = 'mic';
     }
     console.log('nanoleaf-setScene', scene)
     ipcRenderer.invoke('nanoleaf-setScene', scene);
     ipcRenderer.invoke('electron-icon-overlay', icon);
-  }, [voiceSettings, voiceChannelId]);
+  }, [defaultScene, muteScene, unmuteScene, voiceSettings, voiceChannelId]);
 
   return (
-    <div>
+    <div className="bg-green-200">
       <h2>Nanoleaf</h2>
+      <Table>
+        <thead>
+          <Row>
+            <Data th>Condition</Data>
+            <Data th>Scene</Data>
+          </Row>
+        </thead>
+        <tbody>
+          <Row
+            selected={selectionFn === setDefaultScene}
+            onClick={() => setSelectionFn(() => setDefaultScene)}>
+            <Data>Default</Data>
+            <Data>{defaultScene}</Data>
+          </Row>
+          <Row
+            selected={selectionFn === setUnmuteScene}
+            onClick={() => setSelectionFn(() => setUnmuteScene)}>
+            <Data>Speaking</Data>
+            <Data>{unmuteScene}</Data>
+          </Row>
+          <Row
+            selected={selectionFn === setMuteScene}
+            onClick={() => setSelectionFn(() => setMuteScene)}>
+            <Data>Muted</Data>
+            <Data>{muteScene}</Data>
+          </Row>
+        </tbody>
+      </Table>
       {scenes.length && (
         <div>
           <h3>Scenes</h3>
-          {scenes.map((s) => <p>{s}</p>)}
+          <div className="flex flex-wrap">
+            {scenes.map((s) => (
+              <Button
+                key={s}
+                onClick={selectionFn
+                  ? () => {
+                    console.log(':::: button click', s)
+                    selectionFn(s);
+                    setSelectionFn(null);
+                  }
+                  : undefined
+                }
+              >{s}</Button>
+            ))}
+          </div>
         </div>
       )}
     </div>
   )
 }
+
+const Table = ({ children }) => (
+  <table className="m-1 border border-green-500">{children}</table>
+)
+const Row = ({ thead = false, selected = false, children, ...props }) => {
+  const Component = thead ? 'th' : 'tr';
+  return (
+    <Component className={cx('border border-green-500 cursor-pointer hover:bg-green-500', {
+      'bg-green-600': selected,
+    })} {...props}>{children}</Component>
+  )
+}
+const Data = ({ th = false, children, ...props }) => {
+  const Component = th ? 'th' : 'td';
+  return (
+    <Component className="p-1 border border-green-500" {...props}>{children}</Component>
+  )
+}
+
+const Button = ({ className, onClick, children, ...props }) => (
+  <div
+    className={cx(className, 'm-1 p-2 rounded bg-green-400 cursor-default', {
+      'cursor-pointer transition hover:bg-green-500': !!onClick,
+    })}
+    onClick={onClick}
+    {...props}>
+    {children}
+  </div>
+)
 
 export default Nanoleaf;
